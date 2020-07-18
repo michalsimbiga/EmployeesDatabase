308234f877155862b8ed8d1ea23e90c15f085778
Michal Simbiga <michal.simbiga@gmail.com>
2020-07-18 21:52:35 +0200

    Add repository implementation tests

---
+++ data/src/test/java/com/employeesdatabase/EmployeesRepositoryImplTest.kt
@@ -0,0 +1,94 @@
+package com.employeesdatabase
+
+import com.employeesdatabase.dataSource.LocalDataSource
+import com.employeesdatabase.models.*
+import com.employeesdatabase.repository.EmployeesRepositoryImpl
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+
+class EmployeesRepositoryImplTest {
+
+    private lateinit var repository: EmployeesRepositoryImpl
+
+    private val localDataSource: LocalDataSource = mockk(relaxed = true)
+
+    private val mockedEmployeeList: List<EmployeeEntity> = mockk(relaxed = true)
+    private val mockedEmployee: Employee = mockk(relaxed = true)
+    private val mockedAddress: Address = mockk(relaxed = true)
+
+    @Before
+    fun setup() {
+        repository = EmployeesRepositoryImpl(localDataSource = localDataSource)
+    }
+
+    @Test
+    fun `When getAllEmployees invoked call local data source getAllEmployees method`() =
+        runBlocking {
+
+            coEvery { localDataSource.getAllEmployees() } returns mockedEmployeeList
+
+            repository.getAllEmployees()
+
+            coVerify { localDataSource.getAllEmployees() }
+        }
+
+    @Test
+    fun `When getEmployeeById invoked call local data source getEmployeeById method`() =
+        runBlocking {
+            val mockedEmployeeId = 4L
+
+            coEvery { localDataSource.getEmployeeById(any()) } returns mockedEmployee.toEntity()
+
+            repository.getEmployeeById(mockedEmployeeId)
+
+            coVerify { localDataSource.getEmployeeById(mockedEmployeeId) }
+        }
+
+    @Test
+    fun `When insertEmployee invoked call local data source insertEmployee method`() =
+        runBlocking {
+
+            coEvery { localDataSource.insertEmployee(any()) } returns Unit
+
+            repository.insertEmployee(mockedEmployee)
+
+            coVerify { localDataSource.insertEmployee(mockedEmployee.toEntity()) }
+        }
+
+    @Test
+    fun `When updateEmployee invoked call local data source updateEmployee method`() =
+        runBlocking {
+
+            coEvery { localDataSource.updateEmployee(any()) } returns Unit
+
+            repository.updateEmployee(mockedEmployee)
+
+            coVerify { localDataSource.updateEmployee(mockedEmployee.toEntity()) }
+        }
+
+    @Test
+    fun `When deleteEmployee invoked call local data source deleteEmployee method`() =
+        runBlocking {
+
+            coEvery { localDataSource.deleteEmployee(any()) } returns Unit
+
+            repository.deleteEmployee(mockedEmployee)
+
+            coVerify { localDataSource.deleteEmployee(mockedEmployee.toEntity()) }
+        }
+
+    @Test
+    fun `When deleteAddress invoked call local data source getEmployeeById method`() =
+        runBlocking {
+
+            coEvery { localDataSource.deleteAddress(any()) } returns Unit
+
+            repository.deleteAddress(mockedAddress)
+
+            coVerify { localDataSource.deleteAddress(mockedAddress.toEntity()) }
+        }
+}
